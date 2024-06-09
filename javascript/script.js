22c71ec347cb6a763c954ddf7115c96962ebb9b0
miporin <carolsfernandes@outlook.com>
2024-06-09 15:39:33 -0300

    new version

---
+++ javascript/script.js
@@ -0,0 +1,47 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const navLinks = document.querySelectorAll('.nav-link');
+  
+    // Function to remove the 'active' class on mobile breakpoint
+    function handleMobileBreakpoint(e) {
+        if (e.matches) { // If mobile breakpoint is matched
+            navLinks.forEach(link => link.classList.remove('active'));
+        }
+    }
+  
+    // Create a MediaQueryList object
+    const mediaQuery = window.matchMedia('(max-width: 600px)');
+  
+    // Add a listener for changes in the media query
+    mediaQuery.addListener(handleMobileBreakpoint);
+  
+    // Initial check
+    handleMobileBreakpoint(mediaQuery);
+  
+    // Add click event listener to each link
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+  
+          //   event.preventDefault();
+  
+            // Remove 'active' class de todos os links
+            navLinks.forEach(link => link.classList.remove('active'));
+  
+            // Adiciona 'active' class ao link clicado
+            this.classList.add('active');
+        });
+    });
+  });
+  
+  function handleResize() {
+      const images = document.querySelectorAll('.carousel-inner .carousel-item img');
+      if (window.matchMedia('(max-width: 760px)').matches) {
+          images.forEach(img => img.classList.remove('rounded-pill'));
+      } else {
+          images.forEach(img => img.classList.add('rounded-pill'));
+      }
+  }
+  // Chama a função de redimensionamento
+  handleResize();
+  
+  // Adicione o listener para o evento de redimensionamento
+  window.addEventListener('resize', handleResize);
